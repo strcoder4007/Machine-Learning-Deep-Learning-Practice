73116c599266ad3cfd47676be278c21fec293598
Shubham Singh <mynameshubham@gmail.com>
2020-01-20 04:17:40 +0400

    K-Means clustering titanic data

---
+++ kmeans-titanic-clustering.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from sklearn.model_selection import cross_validate as cross_validation
+import pandas as pd
+
+
+'''
+Pclass Passenger Class (1 = 1st; 2 = 2nd; 3 = 3rd)
+survival Survival (0 = No; 1 = Yes)
+name Name
+sex Sex
+age Age
+sibsp Number of Siblings/Spouses Aboard
+parch Number of Parents/Children Aboard
+ticket Ticket Number
+fare Passenger Fare (British pound)
+cabin Cabin
+embarked Port of Embarkation (C = Cherbourg; Q = Queenstown; S = Southampton)
+boat Lifeboat
+body Body Identification Number
+home.dest Home/Destination
+'''
+
+df = pd.read_excel('titanic.xls')
+print(df.head())
