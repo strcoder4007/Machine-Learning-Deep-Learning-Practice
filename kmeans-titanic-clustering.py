3ad16588bd98ad8dd9bbf80a23b848d76528d981
Shubham <mynameshubham@gmail.com>
2020-01-21 02:55:22 +0530

    Custom K-Means

--- kmeans-titanic-clustering.py
+++ kmeans-titanic-clustering.py
@@ -2,28 +2,71 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
 import numpy as np
-from sklearn.cluster import KMeans
-from sklearn import preprocessing
-from sklearn.model_selection import cross_validate as cross_validation
-import pandas as pd
+
+data = np.array([[1, 2],
+                 [1.5, 1.8],
+                 [5, 8],
+                 [8, 8],
+                 [1, 0.6],
+                 [9, 11]])
+
+plt.scatter(X[:, 0], X[:, 1], s=100, linewidths=5)
+plt.show()
+
+colors = 10 * ["g", "r", "c", "b", "k"]
 
 
-'''
-Pclass Passenger Class (1 = 1st; 2 = 2nd; 3 = 3rd)
-survival Survival (0 = No; 1 = Yes)
-name Name
-sex Sex
-age Age
-sibsp Number of Siblings/Spouses Aboard
-parch Number of Parents/Children Aboard
-ticket Ticket Number
-fare Passenger Fare (British pound)
-cabin Cabin
-embarked Port of Embarkation (C = Cherbourg; Q = Queenstown; S = Southampton)
-boat Lifeboat
-body Body Identification Number
-home.dest Home/Destination
-'''
+class K_Means:
+    def __init__(self, k=2, tol=0.001, max_iter=300):
+        self.k = k
+        self.tol = tol
+        self.max_iter = max_iter
 
-df = pd.read_excel('titanic.xls')
-print(df.head())
+    def fit(self, data):
+        self.centroids = {}
+        for i in range(self.k):
+            self.centroids[i] = data[i]
+        for i in range(self.max_iter):
+            self.classifications = {}
+
+            for ii in range(self.k):
+                self.classifications[ii] = []
+
+            for featureset in data:
+                distances = [np.linalg.norm(featureset - self.centroids[centroid]) for centroid in self.centroids]
+                classification = distances.index(min(distances))
+                self.classifications[classification].append(featureset)
+
+            prev_centroids = dict(self.centroids)
+
+            for classification in self.classifications:
+                pass
+            # self.centroids[classification] = np.average(self.classifications[classification], axis = 0)
+
+            optimized = True
+
+            for c in self.centroids:
+                original_centroid = prev_centroids[c]
+                current_centroid = self.centroids[c]
+                if np.sum((current_centroid - original_centroid) / original_centroid * 100.0) > self.tol:
+                    optimized = True
+
+            if optimized:
+                break
+
+    def predict(self, data):
+        distances = [np.linalg.norm(data - self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
+
+
+clf = K_Means()
+clf.fit()
+
+for centroid in clf.centroids:
+    plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1], marker="0", color="k", s=150, linewidth=5)
+
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0], featureset[1], marker="x", color=color, s=150, linewidth=150)
