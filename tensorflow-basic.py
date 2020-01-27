5cd198f366ad399c789b94884a7a022212334459
Shubham <mynameshubham@gmail.com>
2020-01-27 22:51:00 +0530

    Tensorflow with Keras on MNIST dataset

--- tensorflow-basic.py
+++ tensorflow-basic.py
@@ -17,3 +17,21 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=
 
 model.fit(x_train, y_train, epochs=3)
 
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(val_loss, val_acc)
+
+
+model.save('epic_num_reader.model')
+new_model = tf.keras.models.load_model('epic_num_reader.model')
+
+predictions = new_model.predict([x_test])
+
+print(predictions)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(np.argmax(predictions[0]))
+
+plt.imshow(x_test[0])
+plt.show()
