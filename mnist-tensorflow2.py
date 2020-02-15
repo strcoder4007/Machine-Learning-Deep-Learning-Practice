04f7984225bfb8e6f2ad943fe7bcc7ace05a54a9
Shubham <mynameshubham@gmail.com>
2020-02-15 20:58:45 +0530

    building the neural net

--- mnist-tensorflow2.py
+++ mnist-tensorflow2.py
@@ -14,3 +14,37 @@ for i in range(16):
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
 plt.show()
+
+IMG_SIZE = (28, 28, 1)
+input_img = layers.Input(shape=IMG_SIZE)
+
+model = layers.Conv2D(32, (3, 3), padding='same')(input_img)
+model = layers.Activation('relu')(model)
+model = layers.Conv2D(32, (3, 3), padding='same', strides=(2, 2))(model)
+model = layers.Activation('relu')(model)
+
+model = layers.Conv2D(64, (3, 3), padding='same')(model)
+model = layers.Activation('relu')(model)
+model = layers.Conv2D(64, (3, 3), padding='same', strides=(2, 2))(model)
+model = layers.Activation('relu')(model)
+
+model = layers.Conv2D(64, (3, 3), padding='same')(model)
+model = layers.Activation('relu')(model)
+model = layers.Conv2D(64, (3, 3), padding='same')(model)
+model = layers.Activation('relu')(model)
+
+model = layers.GlobalAveragePooling2D()(model)
+model = layers.Dense(32)(model)
+model = layers.Activation('relu')(model)
+model = layers.Dense(10)(model)
+output_img = layers.Activation('softmax')(model)
+
+model = models.Model(input_img, output_img)
+
+model.summary()
+
+
+
+
+
+
