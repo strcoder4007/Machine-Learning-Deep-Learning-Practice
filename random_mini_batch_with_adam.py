5344efa73d35a51ad3226f82099797a2beed97c6
Shubham <mynameshubham23@gmail.com>
2024-02-21 21:25:21 +0530

    Completed random mini batch optimization

--- random_mini_batch_with_adam.py
+++ random_mini_batch_with_adam.py
@@ -9,10 +9,16 @@ import sklearn.datasets
 
 
 
-%matplotlib inline
+# %matplotlib inline
 plt.rcParams['figure.figsize'] = (7.0, 4.0)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
+
+def sigmoid(Z):
+    return 1/(1+np.exp(-Z))
+
+def relu(Z):
+    return np.maximum(0, Z)
 
 def update_parameters_with_gd(parameters, grads, learning_rate=0.01):
     L = len(parameters)//2
@@ -25,7 +31,7 @@ def random_mini_batch(X, Y, mini_batch_size=32):
     m = X.shape[1]
     mini_batches = []
 
-    permutation = list(np.permutation(m))
+    permutation = list(np.random.permutation(m))
     X_shuffled = X[:, permutation]
     Y_shuffled = Y[:, permutation].reshape((1, m))
 
@@ -103,8 +109,6 @@ def update_parameters_with_adam(parameters, grads, v, s, t, beta1=0.9, beta2=0.9
 
 def load_dataset():
     train_X, train_Y = sklearn.datasets.make_moons(n_samples=300, noise=0.2)
-    print(train_X.shape, train_X)
-    print(train_X[:, 0])
     plt.scatter(train_X[:, 0], train_X[:,1], c=train_Y, s=40, cmap=plt.cm.Spectral)
     train_X = train_X.T
     train_Y = train_Y.reshape((1, train_Y.shape[0]))
@@ -124,12 +128,49 @@ def calculate_cost(a3, Y):
     return cost_total
 
 def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    b1 = parameters['b1']    
+    W2 = parameters['W2']    
+    b2 = parameters['b2']    
+    W3 = parameters['W3']    
+    b3 = parameters['b3']
+
+    z1 = np.dot(W1, X) + b1
+    a1 = relu(z1)
+    z2 = np.dot(W2, a1) + b2
+    a2 = relu(z2)
+    z3 = np.dot(W3, a2) + b3   
+    a3 = sigmoid(z3)
+
+    cache = (W1, b1, z1, a1, W2, b2, z2, a2, W3, b3, z3, a3)
+    return a3, cache
 
 
-def backward_propagation(X, Y, caches):
+def backward_propagation(X, Y, cache):
+    m = X.shape[1]
+    (W1, b1, z1, a1, W2, b2, z2, a2, W3, b3, z3, a3) = cache
 
+    dz3 = 1./m * (a3 - Y)
+    dW3 = np.dot(dz3, a2.T)
+    db3 = np.sum(dz3, axis=1, keepdims = True)
+    
+    da2 = np.dot(W3.T, dz3)
+    dz2 = np.multiply(da2, np.int64(a2 > 0))
+    dW2 = np.dot(dz2, a1.T)
+    db2 = np.sum(dz2, axis=1, keepdims = True)
+    
+    da1 = np.dot(W2.T, dz2)
+    dz1 = np.multiply(da1, np.int64(a1 > 0))
+    dW1 = np.dot(dz1, X.T)
+    db1 = np.sum(dz1, axis=1, keepdims = True)
+    
+    gradients = {"dz3": dz3, "dW3": dW3, "db3": db3,
+                 "da2": da2, "dz2": dz2, "dW2": dW2, "db2": db2,
+                 "da1": da1, "dz1": dz1, "dW1": dW1, "db1": db1}
+    
+    return gradients
 
-def nn_model(X, Y, layer_dims, optimizer, learning_rate=0.01, mini_batch_size=64, beta=0.9, beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=10000):
+def model(X, Y, layer_dims, optimizer, learning_rate=0.01, mini_batch_size=64, beta=0.9, beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=10000):
     L = len(layer_dims)
     costs = []
     t = 0
@@ -180,7 +221,52 @@ def nn_model(X, Y, layer_dims, optimizer, learning_rate=0.01, mini_batch_size=64
 
     return parameters
 
+def predict(X, y, parameters):
+    m = X.shape[1]
+    p = np.zeros((1,m), dtype = np.int64)
+    a3, caches = forward_propagation(X, parameters)
+    
+    # convert probas to 0/1 predictions
+    for i in range(0, a3.shape[1]):
+        if a3[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+    print("Accuracy: "  + str(np.mean((p[0,:] == y[0,:]))))
+    
+    return p
 
+def predict_dec(parameters, X):
+    # Predict using forward propagation and a classification threshold of 0.5
+    a3, cache = forward_propagation(X, parameters)
+    predictions = (a3 > 0.5)
+    return predictions
+
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.show()
 
 
 train_X, train_Y = load_dataset()
+layer_dims = [train_X.shape[0], 5, 2, 1]
+parameters = model(train_X, train_Y, layer_dims, optimizer='gradient_descent')
+predictions = predict(train_X, train_Y, parameters)
+
+plt.title("model with gradient descent optimization")
+axes = plt.gca()
+axes.set_xlim([-1.5, 2.5])
+axes.set_ylim([-1, 1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
