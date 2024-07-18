aeb8dd714eb2cd6ca236c8c7c6cf21662e7b1a29
Shubham <mynameshubham23@gmail.com>
2024-07-18 18:28:24 +0530

    Conv Net using pytorch

--- pygame/main.py
+++ pygame/main.py
@@ -15,20 +15,29 @@ game_display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("BLOB WORLD")
 clock = pygame.time.Clock()
 
+
+class BlueBlob(Blob):
+    def __init__(self, color, x_boundary, y_boundary):
+        super().__init__(color, x_boundary, y_boundary)
+        # self.color = BLUE
+    def move_fast(self):
+        self.x += random.randrange(-7, 7)
+        self.y += random.randrange(-7, 7)
+
 def draw_environment(blob_list):
     game_display.fill(WHITE)
     for blob_dict in blob_list:
         for blob_id in blob_dict:
             blob = blob_dict[blob_id]
             pygame.draw.circle(game_display, blob.color, [blob.x, blob.y], blob.size)
-            blob.move()
+            blob.move_fast()
             blob.check_bounds()
     pygame.display.update()
 
 
 def main():
-    blue_blobs = dict(enumerate([Blob(BLUE, WIDTH, HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
-    red_blobs = dict(enumerate([Blob(RED, WIDTH, HEIGHT) for i in range(STARTING_RED_BLOBS)]))
+    blue_blobs = dict(enumerate([BlueBlob(BLUE, WIDTH, HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
+    red_blobs = dict(enumerate([BlueBlob(RED, WIDTH, HEIGHT) for i in range(STARTING_RED_BLOBS)]))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
